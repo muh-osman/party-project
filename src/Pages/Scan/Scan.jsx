9afeef870cac90395a743451bd218b8eaa05698e
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-09-16 08:53:29 +0300

    Fix

--- src/Pages/Scan/Scan.jsx
+++ src/Pages/Scan/Scan.jsx
@@ -1,23 +1,19 @@
 // QR Code npm
 import { QrReader } from "react-qr-reader";
 // React
-import { useState, useEffect } from "react";
+import { useState } from "react";
 // Scss
 import css from "./Scan.module.scss";
 
 export default function Scan() {
   const [scanResult, setScanResult] = useState(null);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
 
-  useEffect(() => {
-    if (scanResult) {
-      const parts = scanResult.split(/name:|email:/);
-      setName(parts[1]?.trim());
-      setEmail(parts[2]?.trim());
-    }
-    console.log(scanResult);
-  }, [scanResult]);
+
+  try {
+    
+  } catch (error) {
+    
+  }
 
   return (
     <div className={css.container}>
