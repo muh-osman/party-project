5d315ecceab4fe3789874778bf49fc3b70504a86
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-09-21 23:59:47 +0300

    Fix

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,62 @@
-export default function Dashboard() {
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Badge from "@mui/material/Badge";
+
+function createData(name, email, date, isAttended) {
+  return { name, email, date, isAttended };
+}
+
+const rows = [
+  createData("John Doe", "johndoe@gmail.com", "14/8/2025", true),
+  createData("John Doe", "johndoe@gmail.com", "14/8/2025", false),
+  createData("John Doe", "johndoe@gmail.com", "14/8/2025", true),
+  createData("John Doe", "johndoe@gmail.com", "14/8/2025", true),
+  createData("John Doe", "johndoe@gmail.com", "14/8/2025", false),
+  createData("John Doe", "johndoe@gmail.com", "14/8/2025", true),
+  createData("John Doe", "johndoe@gmail.com", "14/8/2025", false),
+  createData("John Doe", "johndoe@gmail.com", "14/8/2025", false),
+];
+
+export default function BasicTable() {
   return (
-    <div>Dashboard</div>
-  )
+    <TableContainer component={Paper}>
+      <Table
+        sx={{
+          minWidth: 650,
+          "& .MuiTableRow-root:hover": {
+            backgroundColor: "#F6F6F6",
+          },
+        }}
+        aria-label="simple table"
+      >
+        <TableHead>
+          <TableRow sx={{ backgroundColor: "#F6F6F6" }}>
+            <TableCell sx={{ fontWeight: "600" }}>Name:</TableCell>
+            <TableCell sx={{ fontWeight: "600" }}>Email:</TableCell>
+            <TableCell sx={{ fontWeight: "600" }}>Register date:</TableCell>
+            <TableCell sx={{ fontWeight: "600" }}>Status:</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.email}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">{row.name}</TableCell>
+              <TableCell>{row.email}</TableCell>
+              <TableCell>{row.date}</TableCell>
+              <TableCell sx={{color: row.isAttended? "#36A2EB" : "#FF6384"}}>{row.isAttended? "Attended" : "Not Attend"}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -5,6 +5,13 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 // Sass
 import css from "./Statistics.module.scss"
+// MUI
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 
 
@@ -16,8 +23,8 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 export default function Statistics() {
 
   const [userNum, setUserNum] = useState({
-    attended: 12,
-    notAttend: 19
+    attended: 170,
+    notAttend: 30
   })
 
    const data = {
@@ -53,9 +60,54 @@ export default function Statistics() {
 
   return (
     <div className={css.container}>
-      <div className={css.box}>
+
+        <div className={css.card_box}>
+
+            <Card sx={{ width: 175, height: 175, backgroundColor: "rgba(255, 206, 86, 1)" }}>
+          <CardActionArea sx={{height: "100%"}}>
+            <CardContent sx={{height: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+              <Typography variant="h4" component="div">
+                {userNum.attended + userNum.notAttend}
+              </Typography>
+              <Typography variant="h6" color="text.secondary">
+              All
+          </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+            <Card sx={{ width: 175, height: 175, backgroundColor: "#36A2EB" }}>
+          <CardActionArea sx={{height: "100%"}}>
+            <CardContent sx={{height: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+              <Typography variant="h4" component="div">
+                {userNum.attended}
+              </Typography>
+              <Typography variant="h6" color="text.secondary">
+              Attended
+          </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+            <Card sx={{ width: 175, height: 175, backgroundColor: "#FF6384" }}>
+          <CardActionArea sx={{height: "100%"}}>
+            <CardContent sx={{height: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+              <Typography variant="h4" component="div">
+                {userNum.notAttend}
+              </Typography>
+              <Typography variant="h6" color="text.secondary">
+              Not Attend
+          </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        </div>
+
+        <div className={css.box}>
           <Doughnut data={data} options={{ responsive: true }} />
-      </div>
+       </div>
+
     </div>
   )
 }
