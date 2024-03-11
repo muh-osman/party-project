bc7028dfeee8f56288212b08205c4737e69ecb08
muh-osman <mr.muhammed.osman@gmail.com>
2024-03-11 15:43:48 +0300

    Fix

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -1,32 +1,62 @@
 // React
 import * as React from "react";
+import { useState } from "react";
 // React router
 import { Link, useLocation, Outlet } from "react-router-dom";
+// Axios
+import axios from "axios";
+// Cookies
+import { useCookies } from "react-cookie";
 // MUI
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import DataUsageIcon from '@mui/icons-material/DataUsage';
-import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import DataUsageIcon from "@mui/icons-material/DataUsage";
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
 import { Stack } from "@mui/material";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import LoadingButton from "@mui/lab/LoadingButton";
 
-
- const drawerWidth = 240;
-
+const drawerWidth = 240;
 
 function DashboardLayout(props) {
+  const [loading, setLoading] = useState(false);
+  const [cookies, setCookies, removeCookie] = useCookies(["token"]);
+
+  const logOut = async () => {
+    // console.log(cookies.token);
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/user/logout",
+        null,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${cookies.token}`,
+          },
+        }
+      );
+
+      // Remove the "token" cookie entirely
+      removeCookie("token", { path: "/" });
+      
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
 
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -39,12 +69,18 @@ function DashboardLayout(props) {
 
   const drawer = (
     <div>
-            <Toolbar style={{ justifyContent: "center" }}>
+      <Toolbar style={{ justifyContent: "center" }}>
         <Link to="../" style={{ textDecoration: "none", color: "#fff" }}>
           <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
-          <ArrowBackIosIcon sx={{color: "#2486F0", fontSize: "medium"}} />
+            <ArrowBackIosIcon sx={{ color: "#2486F0", fontSize: "medium" }} />
             <Typography
-              style={{ textAlign: "center", justifyContent: "center", fontSize: "16px", color: "#2486F0", margin: 0 }}
+              style={{
+                textAlign: "center",
+                justifyContent: "center",
+                fontSize: "16px",
+                color: "#2486F0",
+                margin: 0,
+              }}
               align="center"
               variant="h4"
               noWrap
@@ -59,38 +95,51 @@ function DashboardLayout(props) {
       <Divider />
 
       <List disablePadding>
-          <ListItem disablePadding button component={Link} to="." selected={pathname === "/admin/dashboard"}>
-            <ListItemButton>
-              <ListItemIcon>
-                <SpaceDashboardIcon />
-              </ListItemIcon>
-              <ListItemText primary={"Dashboard"} />
-            </ListItemButton>
-          </ListItem>
+        <ListItem
+          disablePadding
+          button
+          component={Link}
+          to="."
+          selected={pathname === "/admin/dashboard"}
+        >
+          <ListItemButton>
+            <ListItemIcon>
+              <SpaceDashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Dashboard"} />
+          </ListItemButton>
+        </ListItem>
 
-          <ListItem disablePadding button component={Link} to="statistics" selected={pathname === "/admin/dashboard/statistics"}>
-            <ListItemButton>
-              <ListItemIcon>
-                <DataUsageIcon />
-              </ListItemIcon>
-              <ListItemText primary={"Statistics"} />
-            </ListItemButton>
-          </ListItem>
+        <ListItem
+          disablePadding
+          button
+          component={Link}
+          to="statistics"
+          selected={pathname === "/admin/dashboard/statistics"}
+        >
+          <ListItemButton>
+            <ListItemIcon>
+              <DataUsageIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Statistics"} />
+          </ListItemButton>
+        </ListItem>
       </List>
     </div>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
         position="fixed"
         sx={{
           width: { sm: `calc(100% - ${drawerWidth}px)` },
           ml: { sm: `${drawerWidth}px` },
-          backgroundColor: "#2486F0"
+          backgroundColor: "#2486F0",
         }}
       >
         <Toolbar>
@@ -99,13 +148,32 @@ function DashboardLayout(props) {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
+
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="h6" noWrap component="div">
               Party Event
-          </Typography>
+            </Typography>
+
+            <LoadingButton
+              onClick={logOut}
+              loading={loading}
+              variant="outlined"
+              sx={{ color: "#fff", borderColor: "#fff" }}
+            >
+              Logout
+            </LoadingButton>
+          </div>
         </Toolbar>
       </AppBar>
       <Box
@@ -123,18 +191,23 @@ function DashboardLayout(props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
-          
         </Drawer>
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
           open
         >
@@ -143,16 +216,18 @@ function DashboardLayout(props) {
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
       >
         <Toolbar />
 
         <Outlet />
-
       </Box>
     </Box>
   );
 }
-
 
 export default DashboardLayout;
